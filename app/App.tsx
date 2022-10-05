33f5c79ce6ebc815516a7d1524a4d7ccf242125e
Thien Nt <thiennt3@vng.com.vn>
2022-10-05 21:51:02 +0700

    set up project structure

---
+++ app/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Home from './screens/Home';
+
+
+const App = () => {
+  return (
+   <Home />
+  );
+};
+
+export default App;
---
+++ app/screens/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, SafeAreaView} from 'react-native';
+
+
+const Home = () => {
+    return (
+        <SafeAreaView style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'black'}} />
+      </SafeAreaView>
+    )
+};
+
+export default Home;
