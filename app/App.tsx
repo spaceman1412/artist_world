e4a676f918340b211e5c9f9095cc546e248089a8
sekke276 <tringuyen@gmail.com>
2022-10-11 13:57:56 -0700

    before merge

--- app/App.tsx
+++ app/App.tsx
@@ -1,25 +1,9 @@
-
-import * as React from 'react';
-import Home from './screens/Home';
-import { NavigationContainer} from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import ForgotPassWord from './screens/ForgotPassWord';
-
-
+import React from 'react';
+import Home from '@screens/Home';
 
 const App = () => {
-  const Stack = createNativeStackNavigator();
- 
   return (
-    <NavigationContainer>
-      <Stack.Navigator >
-        <Stack.Screen
-        name='Home'
-        component={Home}
-        />
-
-      </Stack.Navigator>
-    </NavigationContainer>
+   <Home />
   );
 };
 
--- app/screens/Home/index.tsx
+++ app/screens/Home/index.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import {View, SafeAreaView, Button} from 'react-native';
 
 
-const Home = () => {
+const Home = ({navigation}) => {
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={{flex: 1, backgroundColor: 'black'}} />
+      <Button title='Press Me' onPress={() => navigation.navigator('Forgot')}/>
     </SafeAreaView>
   );
 };
