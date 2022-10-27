6fa4903a11001cc3ad49dd77f4251aba70f774ea
CPea <vienpro2506@gmail.com>
2022-10-28 01:24:00 +0700

    add upload image component

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -1,4 +1,7 @@
 export const images = {
   girl: require('./girl.png'),
+  placeholder: require('./placeholder.jpg'),
+  camera: require("./camera.svg")
 };
+
 export type ImageTypes = keyof typeof images;
