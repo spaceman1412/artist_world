62da37a8e7c6a3f65f8ee9c6452bd76d8a4005d0
sekke276 <tringuyen@gmail.com>
2022-11-14 09:26:33 +0700

    fix link and style

--- app/components/swiper-image/onboardItem/onboardItem.tsx
+++ app/components/swiper-image/onboardItem/onboardItem.tsx
@@ -1,58 +1,44 @@
-import { color } from '@theme';
+import {color} from '@theme';
 import * as React from 'react';
-import {
-    View, 
-    useWindowDimensions, 
-    Image,
-    StyleSheet,
-    Dimensions,
-    Text
-} from 'react-native';
-import { OnboardingItemProps } from './onboardingItem.props';
+import {View, Image, StyleSheet, Text} from 'react-native';
+import {OnboardingItemProps} from './onboardingItem.props';
 
-
-const WIDTH = Dimensions.get('window').width;
-const HEIGHT = Dimensions.get('window').height;
-export const OnboardingItem = (props: OnboardingItemProps) =>{
-    const {
-        image,
-        name,
-        age,
-        imageType = 'uri',
-    } = props
-    return(
-        <View style={styles.container}>
-            <Image source={{uri: image}} 
-            resizeMode='stretch'
-            style={styles.image}/>
-            <View style={styles.information}>
-                <Text style={styles.name}>{name}, {age}</Text>
-                
-            </View>
-        </View>
-    )
-}
+export const OnboardingItem = (props: OnboardingItemProps) => {
+  const {image, name, age, width = 300, height = 500} = props;
+  const sizeStyle = {width, height};
+  return (
+    <View style={[sizeStyle, styles.container]}>
+      <Image
+        source={image}
+        resizeMode="stretch"
+        style={[sizeStyle, styles.container]}
+      />
+      <View style={styles.information}>
+        <Text style={styles.name}>
+          {name}, {age}
+        </Text>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    image:{
-        width: WIDTH,
-        height: HEIGHT,
-    },
-    container:{
-        width: WIDTH,
-        height: HEIGHT,
-    },
-    information:{
-        width: '100%',
-        height: 180,
-        backgroundColor: color.palette.blackWithOpacity(0.7),
-        position: 'absolute',
-        bottom: 0,
-        padding: 10,
-    },
-    name:{
-        fontSize: 30,
-        color: color.text,
-        fontWeight: '700',
-    }
-})
+  container: {
+    borderRadius: 15,
+  },
+  information: {
+    width: '100%',
+    height: '20%',
+    backgroundColor: color.palette.blackWithOpacity(0.7),
+    position: 'absolute',
+    bottom: 0,
+    padding: 10,
+    borderBottomLeftRadius: 15,
+    borderBottomRightRadius: 15,
+  },
+  name: {
+    fontSize: 30,
+    color: color.text,
+    fontWeight: '700',
+  },
+});
--- app/components/swiper-image/onboardItem/onboardingItem.props.ts
+++ app/components/swiper-image/onboardItem/onboardingItem.props.ts
@@ -1,8 +1,9 @@
+import {ImageSourcePropType} from 'react-native';
 
-
-export interface OnboardingItemProps{
-    image : string,
-    name: string,
-    age: string,
-    imageType?: 'uri' | 'local'
+export interface OnboardingItemProps {
+  image: ImageSourcePropType;
+  name: string;
+  age: string;
+  width: number;
+  height: number;
 }
